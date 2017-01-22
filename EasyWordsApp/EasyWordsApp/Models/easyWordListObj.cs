36302dcf28f4812f5021610bde4d2b771be298c1
Jaykob86 <jaykob86@gmail.com>
2017-01-22 01:36:27 +0100

    new-class-ewObj
    New class file created.

---
+++ EasyWordsApp/EasyWordsApp/Models/easyWordListObj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWordsApp.Models
+{
+    public class easyWordListObj
+    {
+        public List<easyWord> ewList { get; set; }
+        public string ewListName { get; set; }
+
+        public easyWordListObj(string listName = "")
+        {
+            ewList = new List<easyWord>();
+            ewListName = listName;
+        }
+
+
+        public class easyWord
+        {
+            public string ewLeftSide { get; set; }
+            public string ewRightSide { get; set; }
+            public int ewKnowledgeLevel { get; set; }
+
+            public easyWord(string leftSide = "", string rightSide = "")
+            {
+                ewLeftSide = leftSide;
+                ewRightSide = rightSide;
+                ewKnowledgeLevel = 0;
+            }
+
+        }
+
+    }
+
+}
